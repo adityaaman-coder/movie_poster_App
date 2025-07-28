32c33dd9533a83905ea23803bb4fd882ac5d6124
adityaaman-coder <adityaaman400@gmail.com>
2025-07-28 09:57:20 +0530

    index.js

--- index.js
+++ index.js
@@ -1,35 +1,75 @@
-const express = require('express');
-const cors = require('cors');
-// const fetch = require('node-fetch');
+// MOVIE POSTER SERVER //
 
-// "http://www.omdbapi.com/?apikey=ff35f87a&s=" 
 
+// const express = require('express');
+// const cors = require('cors');
+// // const fetch = require('node-fetch');
+
+// // "http://www.omdbapi.com/?apikey=ff35f87a&s=" 
+
+// const app = express();
+// app.use(cors());
+
+// app.get("/api",async(req , res)=>{
+
+//     var name = req.query.value;
+//     if(name==' '||name==''||name=='.')
+//     {
+//         console.log("something went wrong");
+//         name = 'batman';
+//     }
+    
+//     const url = "http://www.omdbapi.com/?apikey=ff35f87a&s="+name;
+    
+
+//     try{
+//         const response = await fetch(url);
+//         const data = await response.json();
+//         return res.json(data);
+//     } catch(error){
+//         console.log(error+"something went wrong backend");
+//     }
+
+    
+// })
+
+// app.listen(4500,()=>{
+//     console.log("server running at port 4500");
+// })
+
+
+
+//  YOUTUBE CLOSE SERVER //
+
+
+const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(cors());
 
-app.get("/api",async(req , res)=>{
-
+app.get("/movie",async(req , res)=>{
     var name = req.query.value;
-    if(name==' '||name==''||name=='.')
-    {
-        console.log("something went wrong");
-        name = 'batman';
-    }
-    
-    const url = "http://www.omdbapi.com/?apikey=ff35f87a&s="+name;
-    
-
+    const url = "https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyDgbfDh0ugI2tFCTlEaHre9wigPFc273OA&maxResults=5&type=video&q="+name;
     try{
         const response = await fetch(url);
         const data = await response.json();
         return res.json(data);
-    } catch(error){
-        console.log(error+"something went wrong backend");
+    }catch(error){
+        console.log(error);
     }
-
-    
+})
+       
+app.listen(5500,()=>{
+    console.log('server running at port 5500');
 })
 
-app.listen(4500,()=>{
-    console.log("server running at port 4500");
-})
+
+
+
+
+
+
+
+
+
+
